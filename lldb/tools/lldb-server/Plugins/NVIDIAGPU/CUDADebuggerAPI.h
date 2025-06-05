126bcb944a2d68d2e130160bcba1c834a322ffb8
Walter Erquinigo <werquinigo@nvidia.com>
2025-06-05 15:40:35 +0000

    Add NVIDIA GPU plugin

---
+++ lldb/tools/lldb-server/Plugins/NVIDIAGPU/CUDADebuggerAPI.h
@@ -0,0 +1,56 @@
+//===-- CUDADebuggerAPI.h ---------------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLDB_TOOLS_LLDB_SERVER_CUDADDEBUGGERAPI_H
+#define LLDB_TOOLS_LLDB_SERVER_CUDADDEBUGGERAPI_H
+
+#include "cudadebugger.h"
+#include "lldb/Host/common/NativeProcessProtocol.h"
+
+#include <memory>
+
+namespace lldb_private::lldb_server {
+
+/// Custom deleter for CUDBGAPI.
+void CUDBGAPIDeleter(CUDBGAPI api);
+
+/// RAII wrapper class for CUDA debugger API instances.
+/// The API methods are accessed through the -> operator.
+class CUDADebuggerAPI {
+public:
+  /// The name of the CUDA library that contains the CUDA debugger API.
+  static constexpr const char *CUDA_API_LIBRARY_NAME = "libcuda.so.1";
+
+  /// Initialize the CUDA debugger API.
+  ///
+  /// \param bp_args The GPU plugin breakpoint that triggered the
+  /// initialization.
+  /// \param linux_process The Linux process that spawned the GPU process.
+  static llvm::Expected<CUDADebuggerAPI>
+  Initialize(const GPUPluginBreakpointHitArgs &bp_args,
+             NativeProcessProtocol &linux_process);
+
+  CUDBGAPI operator->() const { return m_api_up.get(); }
+
+  CUDBGAPI GetRawAPI() const { return m_api_up.get(); }
+
+  static GPUBreakpointInfo GetInitializationBreakpointInfo();
+
+private:
+  CUDADebuggerAPI(CUDBGAPI api) : m_api_up(api, CUDBGAPIDeleter) {}
+
+  std::unique_ptr<const CUDBGAPI_st, decltype(&CUDBGAPIDeleter)> m_api_up;
+
+  static llvm::Expected<CUDADebuggerAPI>
+  InitializeImpl(const GPUPluginBreakpointHitArgs &bp_args,
+                 NativeProcessProtocol &linux_process);
+};
+
+} // namespace lldb_private::lldb_server
+
+#endif // LLDB_TOOLS_LLDB_SERVER_CUDADDEBUGGERAPI_H
---
+++ lldb/tools/lldb-server/Plugins/NVIDIAGPU/MainLoopEventNotifier.h
@@ -0,0 +1,70 @@
+//===-- MainLoopFDNotifier.h ------------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLDB_PLUGINS_NVIDIAGPU_MAINLOOPEVENTNOTIFIER_H
+#define LLDB_PLUGINS_NVIDIAGPU_MAINLOOPEVENTNOTIFIER_H
+
+#include <sys/socket.h>
+
+#include "lldb/Host/MainLoop.h"
+
+namespace lldb_private::lldb_server {
+
+class MainLoopEventNotifier;
+using MainLoopEventNotifierUP = std::unique_ptr<MainLoopEventNotifier>;
+
+/// This class is used to notify a main loop that an event has occurred.
+/// It uses a file descriptor pair to communicate with the main loop, which is
+/// in a different thread. The file descriptor pair is probably the fastest way
+/// to achieve this inter-thread communication.
+class MainLoopEventNotifier {
+public:
+  ~MainLoopEventNotifier();
+
+  /// Create a notifier for an event callback registering it to the given main
+  /// loop.
+  ///
+  /// \param name The name of the notifier for error reporting purposes.
+  /// \param main_loop The main loop to register the notifier to.
+  /// \param callback The callback to invoke when the event is fired.
+  ///
+  /// \returns A unique pointer to the notifier or an error.
+  static llvm::Expected<MainLoopEventNotifierUP>
+  CreateForEventCallback(llvm::StringRef name, MainLoop &main_loop,
+                         std::function<void()> callback);
+
+  /// Fire an event. This will invoke the callback.
+  void FireEvent();
+
+private:
+  MainLoopEventNotifier(llvm::StringRef name, std::function<void()> callback);
+
+  void InvokeCallback();
+
+  int GetReadFD() const { return m_fds[0]; }
+  int GetWriteFD() const { return m_fds[1]; }
+
+  lldb::FileSP GetReadFile() const { return m_files[0]; }
+  lldb::FileSP GetWriteFile() const { return m_files[1]; }
+
+  /// The name of the notifier.
+  std::string m_name;
+  /// Raw fds for the notifier.
+  int m_fds[2] = {-1, -1};
+  /// Corresponding FileSPs for the fds. The MainLoop class wants these objects.
+  lldb::FileSP m_files[2] = {nullptr, nullptr};
+  /// The handle gotten after registering an event handler in the MainLoop
+  /// class.
+  MainLoop::ReadHandleUP m_debugger_api_main_loop_handle;
+  /// The callback to invoke when the event is fired.
+  std::function<void()> m_callback;
+};
+
+} // namespace lldb_private::lldb_server
+
+#endif // LLDB_PLUGINS_NVIDIAGPU_MAINLOOPEVENTNOTIFIER_H
