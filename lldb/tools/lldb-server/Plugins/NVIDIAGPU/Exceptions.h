aa1e357316a2026322a052e316fb2cf8a8fce38b
Walter Erquinigo <werquinigo@nvidia.com>
2025-07-18 11:52:00 -0700

    [LLDB][NVIDIA] Show exceptions as strings
    With this, now we show more descriptive exceptions.

--- lldb/tools/lldb-server/Plugins/NVIDIAGPU/Exceptions.h
+++ lldb/tools/lldb-server/Plugins/NVIDIAGPU/Exceptions.h
@@ -11,6 +11,7 @@
 
 #include "Coords.h"
 #include "cudadebugger.h"
+#include "llvm/ADT/StringRef.h"
 #include <optional>
 
 namespace lldb_private::lldb_server {
@@ -29,6 +30,15 @@ struct ExceptionInfo {
 /// \return
 //    The exception info if found, otherwise \b std::nullopt.
 std::optional<ExceptionInfo> FindExceptionInfo(const CUDBGAPI_st &api);
+
+/// Convert a CUDA exception to a string.
+///
+/// \param[in] exception
+//    The CUDA exception.
+///
+/// \return The string representation of the exception.
+llvm::StringRef CudaExceptionToString(CUDBGException_t exception);
+
 } // namespace lldb_private::lldb_server
 
 #endif // LLDB_TOOLS_LLDB_SERVER_PLUGINS_NVIDIAGPU_EXCEPTIONS_H
