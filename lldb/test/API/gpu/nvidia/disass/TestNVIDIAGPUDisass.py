2ac730ccd4a348040e521932191754d7cc2f88d9
Walter Erquinigo <werquinigo@nvidia.com>
2025-08-05 08:59:03 -0700

    [LLDB][NVIDIA] Fix the length of each instruction in disass
    I noticed when comparing the disass of cuda-gdb that each instruction is
    twice the size than the one being used. For example
    
    ```
    (cuda-gdb) disass
    Dump of assembler code for function _Z10assert_onejj:
       0x00007fffd7264000 <+0>:     MOV R1,c[0x0][0x28]
       0x00007fffd7264010 <+16>:    MOV R6,c[0x0][0x118]
       0x00007fffd7264020 <+32>:    MOV R7,c[0x0][0x11c]
       0x00007fffd7264030 <+48>:    MOV R0,RZ
       0x00007fffd7264040 <+64>:    LDC R0,c[0x0][R0+0x160]
    ```
    
    The original disass code in LLDB was having offsets like +0, +8, +16,
    +24 for the same instructions.

--- lldb/test/API/gpu/nvidia/disass/TestNVIDIAGPUDisass.py
+++ lldb/test/API/gpu/nvidia/disass/TestNVIDIAGPUDisass.py
@@ -8,10 +8,20 @@ class TestNVIDIAGPUDisass(TestBase):
         """Test that we can disassemble an ELFv7 binary."""
         self.runCmd("file " + self.getSourcePath("elfv7.cubin"))
         self.expect(
-            "disassemble -a 0x00007fffd7243b00", substrs=["elfv7.cubin[0x7fffd7243b00] <+0>:", "MOV", "R1,c[0x0][0x28]"]
+            "disassemble -a 0x00007fffd7243b00",
+            patterns=[
+                "elfv7.cubin\\[0x7fffd7243b00\\].*<\\+0>:.*MOV.*R1,c\\[0x0\\]\\[0x28\\]",
+                "elfv7.cubin\\[0x7fffd7243b10\\].*<\\+16>:.*S2R.*R2,SR_TID.X",
+            ],
         )
 
     def test_disass_elf_v8(self):
         """Test that we can disassemble an ELFv8 binary."""
         self.runCmd("file " + self.getSourcePath("elfv8.cubin"))
-        self.expect("disassemble -a 0x00007fffcf280300", substrs=["elfv8.cubin[0x7fffcf280300] <+0>:", "MOV", "R4,R4"])
+        self.expect(
+            "disassemble -a 0x00007fffcf280300",
+            patterns=[
+                "elfv8.cubin\\[0x7fffcf280300\\].*<\\+0>:.*MOV.*R4,R4",
+                "elfv8.cubin\\[0x7fffcf280310\\].*<\\+16>:.*FADD.*R3,-RZ,|R4|",
+            ],
+        )
