32c7a9e8c28885f7ed5e30b909ffadc1ec01bbe0
Walter Erquinigo <werquinigo@nvidia.com>
2025-08-25 10:18:22 -0700

    [LLDB][NVIDIA] Add support for multiple address spaces
    This adds support for reading from multiple address spaces. I was only
    able to easily test global and generic, which fortunately execute most
    code paths.
    
    Other address spaces will naturally be tested by the unwinder. It's hard
    to test them without supporting first stdout from the GPU or without
    a DWARF parser. However, I did test manually the param memory reading
    logic is working.

--- lldb/test/API/gpu/nvidia/memory/TestNVIDIAGPUMemory.py
+++ lldb/test/API/gpu/nvidia/memory/TestNVIDIAGPUMemory.py
@@ -23,8 +23,20 @@ class TestNVIDIAGPUMemory(NvidiaGpuTestCaseBase):
 
         self.select_gpu()
 
+        # This doesn't require a thread for reading.
         self.expect(
-            f"memory read {d_arr_addr} --format x --size 4 -c 16",
+            f"memory read -p global {d_arr_addr} --format x --size 4 -c 16",
+            substrs=[
+                "0x00000000 0x00000001 0x00000002 0x00000003",
+                "0x00000004 0x00000005 0x00000006 0x00000007",
+                "0x00000008 0x00000009 0x0000000a 0x0000000b",
+                "0x0000000c 0x0000000d 0x0000000e 0x0000000f",
+            ],
+        )
+
+        # This requires a thread for reading.
+        self.expect(
+            f"memory read -p generic {d_arr_addr} --format x --size 4 -c 16",
             substrs=[
                 "0x00000000 0x00000001 0x00000002 0x00000003",
                 "0x00000004 0x00000005 0x00000006 0x00000007",
