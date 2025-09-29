e7b7fd8c195510b499c5f39a64ce9ede6988c572
Walter Erquinigo <werquinigo@nvidia.com>
2025-09-29 10:39:32 -0700

    [LLDB][NVIDIA] Add support for breakpoints
    - Add support for breakpoints
    - The cpu breakpoints get copied to the gpu during connection
    - The selected thread in the server will be one with exception, or otherwise one with a breakpoint

---
+++ lldb/test/API/gpu/nvidia/breakpoints/breakpoints.cu
@@ -0,0 +1,28 @@
+#include <assert.h>
+#include <iostream>
+
+__global__ void breakpoints(int value) {
+  printf("%d\n", value);
+  if (threadIdx.x == 0) {
+    printf("will break %d\n", value); // gpu breakpoint
+    int fault_here = *(volatile int *)0x03;
+
+    if (fault_here)
+      printf("Fault here\n");
+  }
+}
+
+int main(void) {
+  const int N = 16;
+
+  cudaDeviceReset();
+
+  int threadsPerBlock = 4;
+  int blocksPerGrid =
+      (N + threadsPerBlock - 1) / threadsPerBlock; // cpu breakpoint
+  breakpoints<<<blocksPerGrid, threadsPerBlock>>>(22);
+
+  cudaDeviceSynchronize();
+
+  return 0;
+}
