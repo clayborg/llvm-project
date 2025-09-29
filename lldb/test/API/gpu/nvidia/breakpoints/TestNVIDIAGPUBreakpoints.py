e060966ebad891e4f693b61e0f5bc43f50109437
Walter Erquinigo <werquinigo@nvidia.com>
2025-09-29 10:39:32 -0700

    [LLDB][NVIDIA] Add support for breakpoints
    - Add support for breakpoints
    - The cpu breakpoints get copied to the gpu during connection
    - The selected thread in the server will be one with exception, or otherwise one with a breakpoint

---
+++ lldb/test/API/gpu/nvidia/breakpoints/TestNVIDIAGPUBreakpoints.py
@@ -0,0 +1,30 @@
+import lldb
+from lldbsuite.test import lldbutil
+from lldbsuite.test.lldbtest import line_number
+from lldbsuite.test.tools.gpu.nvidiagpu_testcase import NvidiaGpuTestCaseBase
+
+
+class TestNVIDIAGPUBreakpoints(NvidiaGpuTestCaseBase):
+    NO_DEBUG_INFO_TESTCASE = True
+
+    def test_before_kernel_launch(self):
+        """Test that we can set a breakpoint before the kernel launch."""
+        self.build()
+        exe = self.getBuildArtifact("a.out")
+        self.runCmd(f"file {exe}")
+        source = "breakpoints.cu"
+        cpu_bp_line: int = line_number(source, "// cpu breakpoint")
+        gpu_bp_line: int = line_number(source, "// gpu breakpoint")
+
+        self.runCmd(f"b {gpu_bp_line}")
+        self.runCmd(f"b {cpu_bp_line}")
+        self.runCmd("r")
+
+        self.continue_cpu_and_wait_for_gpu_to_stop()
+
+        self.select_gpu()
+
+        self.expect(
+            "thread list",
+            substrs=[f"at {source}:{gpu_bp_line}, name = 'threadIdx(x=0 y=0 z=0)', stop reason = breakpoint"],
+        )
