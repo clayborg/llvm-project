8fac27362df72e23d05369c5b85c1a6d24a10325
Walter Erquinigo <werquinigo@nvidia.com>
2025-10-03 20:41:25 +0000

    [LLDB][NVGPU] Skip single steps on breakpoints upon resuming
    UD handles that for us, so we need a way to LLDB we need that.
    Dmitry said that there's a setting for that, but I really didn't find.
    And because we are using the gdb-remote protocol, it seems we need a
    custom way to skip this logic.
    
    I'll check with Greg and Dave if they want this patch in the shared
    branch or if they prefer a different way to achieve this.

--- lldb/test/API/gpu/nvidia/breakpoints/TestNVGPUBreakpoints.py
+++ lldb/test/API/gpu/nvidia/breakpoints/TestNVGPUBreakpoints.py
@@ -16,10 +16,12 @@ class TestNVGPUBreakpoints(NVGPUTestCaseBase):
         self.runCmd(f"file {exe}")
         source = "breakpoints.cu"
         cpu_bp_line: int = line_number(source, "// cpu breakpoint")
-        gpu_bp_line: int = line_number(source, "// gpu breakpoint")
+        gpu_bp_line: int = line_number(source, "// gpu breakpoint 1")
+        gpu_bp_line_2: int = line_number(source, "// gpu breakpoint 2")
         exit_bp_line: int = line_number(source, "// breakpoint before exit")
 
         self.runCmd(f"b {gpu_bp_line}")
+        self.runCmd(f"b {gpu_bp_line_2}")
         self.runCmd(f"b {cpu_bp_line}")
         self.runCmd(f"b {exit_bp_line}")
         self.runCmd("r")
@@ -31,9 +33,22 @@ class TestNVGPUBreakpoints(NVGPUTestCaseBase):
         self.expect(
             "thread list",
             substrs=[
-                f"at {source}:{gpu_bp_line}, name = 'blockIdx(x=0 y=0 z=0) threadIdx(x=0 y=0 z=0)', stop reason = breakpoint",
-                f"at {source}:{gpu_bp_line}, name = 'blockIdx(x=1 y=0 z=0) threadIdx(x=0 y=0 z=0)', stop reason = breakpoint",
-                f"at {source}:{gpu_bp_line}, name = 'blockIdx(x=2 y=0 z=0) threadIdx(x=0 y=0 z=0)', stop reason = breakpoint",
-                f"at {source}:{gpu_bp_line}, name = 'blockIdx(x=3 y=0 z=0) threadIdx(x=0 y=0 z=0)', stop reason = breakpoint",
+                f"at {source}:{gpu_bp_line}, name = 'blockIdx(x=0 y=0 z=0) threadIdx(x=0 y=0 z=0)', stop reason = breakpoint 1.1",
+                f"at {source}:{gpu_bp_line}, name = 'blockIdx(x=1 y=0 z=0) threadIdx(x=0 y=0 z=0)', stop reason = breakpoint 1.1",
+                f"at {source}:{gpu_bp_line}, name = 'blockIdx(x=2 y=0 z=0) threadIdx(x=0 y=0 z=0)', stop reason = breakpoint 1.1",
+                f"at {source}:{gpu_bp_line}, name = 'blockIdx(x=3 y=0 z=0) threadIdx(x=0 y=0 z=0)', stop reason = breakpoint 1.1",
+            ],
+        )
+
+        self.dbg.SetAsync(False)
+        self.select_gpu()
+        self.gpu_process.Continue()
+        self.expect(
+            "thread list",
+            substrs=[
+                f"at {source}:{gpu_bp_line_2}, name = 'blockIdx(x=0 y=0 z=0) threadIdx(x=0 y=0 z=0)', stop reason = breakpoint 2.1",
+                f"at {source}:{gpu_bp_line_2}, name = 'blockIdx(x=1 y=0 z=0) threadIdx(x=0 y=0 z=0)', stop reason = breakpoint 2.1",
+                f"at {source}:{gpu_bp_line_2}, name = 'blockIdx(x=2 y=0 z=0) threadIdx(x=0 y=0 z=0)', stop reason = breakpoint 2.1",
+                f"at {source}:{gpu_bp_line_2}, name = 'blockIdx(x=3 y=0 z=0) threadIdx(x=0 y=0 z=0)', stop reason = breakpoint 2.1",
             ],
         )
