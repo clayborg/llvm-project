02160c3dc7929aa85fb60b43c2c93678cab149b3
Walter Erquinigo <werquinigo@nvidia.com>
2025-10-14 09:16:15 -0700

    [LLDB][NVGPU] Support two possible libcuda names
    On regular linux, libcuda is found as `libcuda.so.1`, but on android it's found as plain `libcuda.so`. When setting up the initialization breakpoint, we need the exact shared library name. The easiest way to handle this is by creating two breakpoints, one on each variant of the name. LLDB will only report one of them during initialization.

--- lldb/tools/lldb-server/Plugins/NVGPU/CUDADebuggerAPI.h
+++ lldb/tools/lldb-server/Plugins/NVGPU/CUDADebuggerAPI.h
@@ -23,23 +23,28 @@ void CUDBGAPIDeleter(CUDBGAPI api);
 /// The API methods are accessed through the -> operator.
 class CUDADebuggerAPI {
 public:
-  /// The name of the CUDA library that contains the CUDA debugger API.
-  static constexpr const char *CUDA_API_LIBRARY_NAME = "libcuda.so.1";
+  /// The names of the CUDA library that contains the CUDA debugger API.
+  static constexpr const char *LIBCUDA_LIBRARY_NAME = "libcuda.so";
+  static constexpr const char *LIBCUDA_LIBRARY_NAME_ALT = "libcuda.so.1";
 
   /// Initialize the CUDA debugger API.
   ///
   /// \param bp_args The GPU plugin breakpoint that triggered the
   /// initialization.
+  /// \param libcuda_library_name The name of the CUDA library that contains the
+  /// CUDA debugger API.
   /// \param linux_process The Linux process that spawned the GPU process.
   static llvm::Expected<CUDADebuggerAPI>
   Initialize(const GPUPluginBreakpointHitArgs &bp_args,
+             llvm::StringRef libcuda_library_name,
              NativeProcessProtocol &linux_process);
 
   CUDBGAPI operator->() const { return m_api_up.get(); }
 
   CUDBGAPI GetRawAPI() const { return m_api_up.get(); }
 
-  static GPUBreakpointInfo GetInitializationBreakpointInfo();
+  static GPUBreakpointInfo
+  GetInitializationBreakpointInfo(llvm::StringRef library_name);
 
 private:
   CUDADebuggerAPI(CUDBGAPI api) : m_api_up(api, CUDBGAPIDeleter) {}
@@ -48,6 +53,7 @@ private:
 
   static llvm::Expected<CUDADebuggerAPI>
   InitializeImpl(const GPUPluginBreakpointHitArgs &bp_args,
+                 llvm::StringRef libcuda_library_name,
                  NativeProcessProtocol &linux_process);
 };
 
